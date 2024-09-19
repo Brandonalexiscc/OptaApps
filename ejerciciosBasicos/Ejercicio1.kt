528fec713e968e18f3ee1052f028255f466313f4
Brandonalexiscc <bcanales@ucol.mx>
2024-09-19 16:19:09 -0600

    Add: first practice in kotlin
    ejericicios básicos

---
+++ ejerciciosBasicos/Ejercicio1.kt
@@ -0,0 +1,10 @@
+import java.util.*
+
+fun main(){
+    val scanner= Scanner(System.`in`)
+    println("Introduce un valor entero:")
+    val numero=scanner.nextInt()
+    val doble=numero*2
+    val triple = numero*3
+    println("El numero es ${numero} donde el doble es: ${doble} y e ltripe ${triple}")
+}
