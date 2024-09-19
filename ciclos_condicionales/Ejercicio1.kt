dd17fc3324b56c0fc6629bf76e58dbeef4ad8f9b
Brandonalexiscc <bcanales@ucol.mx>
2024-09-19 16:55:58 -0600

    Ejercicios de condicionales y ciclos

---
+++ ciclos_condicionales/Ejercicio1.kt
@@ -0,0 +1,16 @@
+import java.util.*
+// Ejercicio 1 números introducidos hasta el negativo
+fun main(){
+    val scanner = Scanner(System.`in`)
+    println("intruduce el número: ")
+
+    for (i in 1..100) {
+        if(scanner.nextInt() < 0){
+            println("Números introducidos: ${i}")
+            break
+        }else{
+            println("Nuevo número")
+            //scanner.nextInt()
+        }
+    }
+}
