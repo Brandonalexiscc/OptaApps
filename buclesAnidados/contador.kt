3ed7455e94850106cf5aaa723df62be6c5fb2694
Brandonalexiscc <bcanales@ucol.mx>
2024-09-27 12:56:22 -0600

    Ejercicio contador

--- buclesAnidados/contador.kt
+++ buclesAnidados/contador.kt
@@ -1,2 +1,11 @@
 package buclesAnidados
 
+fun main () {
+    for (i in 0..99999) {
+        var numero = i.toString().padStart(5, '0')
+        numero = numero.replace('3', 'E')
+        val formato = numero.toCharArray().joinToString("-")
+
+        println(formato)
+    }
+}
